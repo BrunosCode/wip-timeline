14bf3bbd76eb5fd95be19a81d3f533813e6dcb49
brunEdo <edobruno.m@gmail.com>
2022-01-12 12:47:44 +0100

    filter timeline control

--- src/app/filterTimeline.js
+++ src/app/filterTimeline.js
@@ -1,7 +1,8 @@
 export const filterTimeline = (nodesArray) => {
   // get filter dates
-  let startDate = new Date(document.getElementById("start-date").value);
-  let endDate = new Date(document.getElementById("end-date").value);
+  let startDate = new Date(document.getElementById("start-date").value || "1994-08-29");
+  let endDate = new Date(document.getElementById("end-date").value || "2022-01-11");
+
 
   // return an array of filtered nodes
   return nodesArray.filter((node) => {
